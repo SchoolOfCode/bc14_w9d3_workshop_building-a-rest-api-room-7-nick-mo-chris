f1f54a1254ab111d02b6cba01ed68dd85b44c794
Mohammad Zafar <mzcoder7@gmail.com>
2023-05-10 15:42:26 +0100

    ticket 2a-b

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-import express from "express";
+import express from 'express';
 const app = express();
 const PORT = 3000;
 
-import { getQuotes, addQuote, getRandomQuote, editQuote, deleteQuote } from "./quote.js";
+import {
+	getQuotes,
+	addQuote,
+	getRandomQuote,
+	editQuote,
+	deleteQuote,
+} from './quote.js';
 
 app.use(express.json());
 
-app.get("/", function (req, res) {
-  res.send("Welcome to cwissy.rest");
+app.get('/', function (req, res) {
+	res.send('Welcome to cwissy.rest');
+});
+
+// Write a request handler to return the correct response and perform the correct action when a GET request is received to /api/quotes
+app.get('/api/quotes', async function (req, res) {
+	console.log(req.query);
+	if (req.query.type === 'random') {
+		const randomQuote = await getRandomQuote();
+		// console.log(randomQuote);
+		return res.send(randomQuote.quoteText);
+	}
+	const quotes = await getQuotes();
+	console.log(quotes);
+	// res.send(quotes);
+	res.send(quotes.map(quote => quote.quoteText));
 });
 
 app.listen(PORT, function () {
-  console.log(`Server is now listening on http://localhost:${PORT}`);
+	console.log(`Server is now listening on http://localhost:${PORT}`);
 });
